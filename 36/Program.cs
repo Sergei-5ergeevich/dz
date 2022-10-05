dd45aa592393c56d519f3cc88bfe21534ca750ff
sergei <nyashko47@gmail.com>
2022-10-05 11:14:19 +0500

    task

--- 36/Program.cs
+++ 36/Program.cs
@@ -1,3 +1,40 @@
 ﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0 */
+Console.Clear();
+Random rnd = new Random();
+int [] array = new int [rnd.Next(4,10)];
+
+void FillArray(int[] coll)
+{
+int length = coll.Length;
+int index = 0;
+while(index < length) 
+    {
+        coll[index] = rnd.Next(-50,51);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+    Console.WriteLine();
+}
+
+FillArray(array);
+PrintArray(array);
+
+int sum = 0;
+for(int i = 0; i < array.Length; i++)
+{
+if(i % 2 > 0) sum=sum+array[i];
+}
+
+Console.WriteLine($"Сумма элементов стоящих на нечетных позициях в массиве: {sum}");
--- 38/Program.cs
+++ 38/Program.cs
@@ -1,2 +1,41 @@
 ﻿/* Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 [3 7 22 2 78] -> 76 */
+Console.Clear();
+Random rnd = new Random();
+int[] array = new int[rnd.Next(4, 10)];
+
+void FillArray(int[] coll)
+{
+    int length = coll.Length;
+    int index = 0;
+    while (index < length)
+    {
+        coll[index] = rnd.Next(0, 100);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+    Console.WriteLine();
+}
+
+FillArray(array);
+PrintArray(array);
+
+int max = array[0];
+int min = array[0];
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i] > max) max = array[i];
+    if (array[i] < min) min = array[i];
+}
+
+Console.WriteLine($"Разница между максимальным({max}) и минимальным({min}) элементами массива = {max - min}");
