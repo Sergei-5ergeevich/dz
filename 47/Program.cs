a5be2d36c358d603e5b9a4d83b58626f37774a1f
sergei <nyashko47@gmail.com>
2022-10-09 11:02:35 +0500

    update

--- 47/Program.cs
+++ 47/Program.cs
@@ -3,3 +3,36 @@ m = 3, n = 4.
 0,5 7 -2 -0,2
 1 -3,3 8 -9,9
 8 7,8 -7,1 9 */
+Console.Clear();
+
+Random rnd = new Random();
+
+void FillArray(double[,] coll)
+{
+    for (int m = 0; m < coll.GetLength(0); m++)
+    {
+        for(int n = 0; n < coll.GetLength(1); n++)
+        {
+            coll[m, n] =  Convert.ToDouble(rnd.Next(-10, 10)) + rnd.NextDouble();
+        }
+    }
+}
+
+void PrintArray(double[,] coll) 
+{
+    for(int m = 0; m < coll.GetLength(0); m++)
+    {
+        for(int n = 0; n < coll.GetLength(1); n++)
+        {
+            Console.Write($"{Math.Round(coll[m, n],2)} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+
+double[,] collect = new double[3,4];
+
+
+FillArray(collect);
+PrintArray(collect);
