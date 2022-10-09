21a80d0e1efc6aef65865e1e75f4d3b92e1caa6e
sergei <nyashko47@gmail.com>
2022-10-09 11:32:07 +0500

    update

--- 50/Program.cs
+++ 50/Program.cs
@@ -5,3 +5,48 @@
 5 9 2 3
 8 4 2 4
 17 -> такого числа в массиве нет */
+Console.Clear();
+
+Random rnd = new Random();
+
+void FillArray(int[,] coll)
+{
+    for (int m = 0; m < coll.GetLength(0); m++)
+    {
+        for (int n = 0; n < coll.GetLength(1); n++)
+        {
+            coll[m, n] = rnd.Next(-10, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] coll)
+{
+    for (int m = 0; m < coll.GetLength(0); m++)
+    {
+        for (int n = 0; n < coll.GetLength(1); n++)
+        {
+            Console.Write($"{coll[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] coll = new int[3, 4];
+FillArray(coll);
+PrintArray(coll);
+
+Console.Write("Введите индекс строки: ");
+int indexrow = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите индекс столбца: ");
+int indexcolumn = Convert.ToInt32(Console.ReadLine());
+
+if (indexrow < coll.GetLength(0) && indexcolumn < coll.GetLength(1))
+{
+    Console.WriteLine(coll[indexrow, indexcolumn]);
+}
+else
+{
+    Console.WriteLine("Такой позиции в массиве нет");
+}
+
